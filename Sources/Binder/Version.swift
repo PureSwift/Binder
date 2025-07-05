b97c643846623f9415065739bae82735604fa001
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-05 14:51:10 -0400

    Add `BinderVersion.current`

--- Sources/Binder/Version.swift
+++ Sources/Binder/Version.swift
@@ -5,6 +5,8 @@
 //  Created by Alsey Coleman Miller on 7/5/25.
 //
 
+import SystemPackage
+import Socket
 import CBinder
 
 public typealias BinderVersion = binder_version
@@ -13,11 +15,11 @@ public typealias BinderVersion = binder_version
 
 extension BinderVersion: @retroactive RawRepresentable {
     
-    public init(rawValue: __s32) {
+    public init(rawValue: Int32) {
         self.init(protocol_version: rawValue)
     }
     
-    public var rawValue: RawValue {
+    public var rawValue: Int32 {
         protocol_version
     }
 }
@@ -46,5 +48,49 @@ extension BinderVersion: @retroactive Hashable {
     
     public func hash(into hasher: inout Hasher) {
         hasher.combine(protocol_version)
+    }
+}
+
+// MARK: - IOCTL
+
+public extension BinderVersion {
+    
+    static var current: BinderVersion {
+        get throws(Errno) {
+            try Self.read()
+        }
+    }
+}
+
+internal extension BinderVersion {
+    
+    static func read() throws(Errno) -> BinderVersion {
+        // Open /dev/binder using SystemPackage
+        let fileDescriptor: SocketDescriptor
+        do {
+            let fd = try FileDescriptor.open("/dev/binder", .readOnly)
+            fileDescriptor = SocketDescriptor(rawValue: fd.rawValue)
+        }
+        catch {
+            throw error as! Errno
+        }
+        // Ensure cleanup
+        return try fileDescriptor.closeAfter { () throws(Errno) -> (BinderVersion) in
+            var version = BinderVersion()
+            //try fd.ioctl(raw: CUnsignedLong(BINDER_VERSION), mutablePointer: &version)
+            try fileDescriptor.inputOutput(&version)
+            return version
+        }
+    }
+}
+
+extension BinderVersion: @retroactive IOControlValue {
+    
+    public static var id: BinderType { .binder }
+    
+    public mutating func withUnsafeMutablePointer<Result>(_ body: (UnsafeMutableRawPointer) throws -> (Result)) rethrows -> Result {
+        try Swift.withUnsafeMutableBytes(of: &self) { buffer in
+            try body(buffer.baseAddress!)
+        }
     }
 }
