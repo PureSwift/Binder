acd313e51d0fc10e1e21d44a893a295eb0ad26fb
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-05 14:12:14 -0400

    Update unit tests

--- Tests/BinderTests/BinderTests.swift
+++ Tests/BinderTests/BinderTests.swift
@@ -5,9 +5,12 @@ import CBinder
 @Suite
 struct BinderTests {
     
-    @Test func binderType() async throws {
+    @Test func binderType() throws {
         #expect(BinderType.binder.rawValue == BINDER_TYPE_BINDER)
         #expect(BinderType(rawValue: UInt32(BINDER_TYPE_BINDER)) == .binder)
     }
+    
+    @Test func driverCommandProtocol() throws {
+        #expect(DriverCommandProtocol.transaction == BC_TRANSACTION)
+    }
 }
-
