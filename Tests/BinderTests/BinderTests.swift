075d191a83d9f93027d5b7a75218caacde0f8a66
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-05 22:16:11 -0400

    Update unit tests

--- Tests/BinderTests/BinderTests.swift
+++ Tests/BinderTests/BinderTests.swift
@@ -14,6 +14,7 @@ struct BinderTests {
     @Test func binderType() throws {
         #expect(BinderType.binder.rawValue == BINDER_TYPE_BINDER)
         #expect(BinderType(rawValue: BinderType.RawValue(BINDER_TYPE_BINDER)) == .binder)
+        #expect(BinderType(rawValue: 0) == nil)
     }
     
     @Test func driverCommandProtocol() throws {
@@ -29,5 +30,23 @@ struct BinderTests {
         #expect(DriverReturnProtocol.ok.rawValue == BR_OK.rawValue)
         #expect(DriverReturnProtocol.finished.description == "BR_FINISHED")
         #expect(DriverReturnProtocol(rawValue: 0).description == "DriverReturnProtocol(rawValue: 0)")
+    }
+    
+    @Test func binderCommand() throws {
+        
+        #expect(BinderCommand.version.rawValue == numericCast(BINDER_VERSION))
+        #expect(BinderCommand.writeRead.rawValue == numericCast(BINDER_WRITE_READ))
+        #expect("\(BinderCommand.threadExit)" == "threadExit")
+        #expect(BinderCommand(rawValue: 0) == nil)
+    }
+    
+    @Test func transactionFlags() throws {
+        
+        #expect(([] as TransactionFlags).isEmpty)
+        #expect(([] as TransactionFlags).description == "[]")
+        #expect(([] as TransactionFlags).rawValue == 0)
+        #expect(TransactionFlags.oneWay == TF_ONE_WAY)
+        #expect(BinderCommand.writeRead.rawValue == numericCast(BINDER_WRITE_READ))
+        #expect("\([.oneWay, .rootObject] as TransactionFlags)" == "[.oneWay, .rootObject]")
     }
 }
